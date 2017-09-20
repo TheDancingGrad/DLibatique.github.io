1ee8aca402f7823751cc4e91f04d61f6f88fdb00
Daniel Libatique <daniellibatique@Daniels-MacBook-Pro-4.local>
2017-09-20 13:56:22 -0400

    added blackjack

--- Blackjack.py
+++ Blackjack.py
@@ -5,3 +5,19 @@ class Player(object):
 
 	def add(self,amount):
 		self.bankroll += amount
+
+class Dealer(object):
+	
+	def __init__(self,bankroll=100):
+		self.bankroll = bankroll
+
+	def add(self,amount):
+		self.bankroll += amount
+
+deck = [2,3,4,5,6,7,8,9,10,]
+
+daniel = Player()
+charlie = Dealer()
+
+def player_turn():
+	
